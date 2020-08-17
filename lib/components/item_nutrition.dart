cb858912c2923edb459e193d306739e6b1c3b2dc
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-08-17 11:10:32 +0200

    Created ItemNutrition

---
+++ lib/components/item_nutrition.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ItemNutrition extends StatelessWidget {
+  const ItemNutrition({
+    @required this.topNameLine1,
+    this.topNameLine2,
+    this.bottomName = "gramos",
+    @required this.value,
+  });
+
+  final String topNameLine1;
+  final String topNameLine2;
+  final String bottomName;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Container(
+        color: Colors.grey[900],
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0),
+          child: Column(children: [
+            topNameLine2 != null
+                ? RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                        text: '$topNameLine1\n',
+                        children: [TextSpan(text: '$topNameLine2')]),
+                  )
+                : Text(topNameLine1),
+            Expanded(
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  '$value',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Text(bottomName),
+          ]),
+        ),
+      ),
+    );
+  }
+}
--- lib/components/nutrition.dart
+++ lib/components/nutrition.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:openfoodfacts/model/Nutriments.dart';
 import 'package:openfoodfacts/utils/NutrimentsHelper.dart';
+import 'package:smart_count_calories/components/item_nutrition.dart';
 
 class Nutrition extends StatelessWidget {
   const Nutrition({
@@ -24,222 +25,49 @@ class Nutrition extends StatelessWidget {
         childAspectRatio: 20 / 16,
         shrinkWrap: true,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.grey[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Energía'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${(NutrimentsHelper.getEnergyAsKCal(nutriments) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('calorías'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Energía',
+            bottomName: 'calorias',
+            value: (NutrimentsHelper.getEnergyAsKCal(nutriments) * gramos / 100)
+                .toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.green[700].withOpacity(1),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Grasas'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.fat ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Grasas',
+            value: ((nutriments.fat ?? 0) * gramos / 100).toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.deepOrange[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                        text: 'Grasas\n',
-                        children: [TextSpan(text: 'saturadas')]),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.saturatedFat ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Grasas',
+            topNameLine2: 'saturadas',
+            value: ((nutriments.saturatedFat ?? 0) * gramos / 100)
+                .toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.orangeAccent[700].withOpacity(0.8),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                        text: 'Hidratos de\n',
-                        children: [TextSpan(text: 'carbono')]),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.carbohydrates ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Hidratos de',
+            topNameLine2: 'carbono',
+            value: ((nutriments.carbohydrates ?? 0) * gramos / 100)
+                .toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.deepOrange[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Azúcares'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.sugars ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Azúcares',
+            value: ((nutriments.sugars ?? 0) * gramos / 100).toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.deepOrange[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Fibra'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.fiber ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Fibra',
+            value: ((nutriments.fiber ?? 0) * gramos / 100).toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.deepOrange[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Proteinas'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.proteins ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Proteínas',
+            value:
+                ((nutriments.proteins ?? 0) * gramos / 100).toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.deepOrange[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Sal'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.salt ?? 0) * gramos / 100).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Sal',
+            value: ((nutriments.salt ?? 0) * gramos / 100).toStringAsFixed(2),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Container(
-              color: Colors.deepOrange[900],
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: Column(children: [
-                  Text('Sodio'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${((nutriments.salt ?? 0) * gramos / 100 * 0.4).toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Text('gramos'),
-                ]),
-              ),
-            ),
+          ItemNutrition(
+            topNameLine1: 'Sodio',
+            value: ((nutriments.salt ?? 0) * gramos / 100 * 0.4)
+                .toStringAsFixed(2),
           ),
         ],
       ),
