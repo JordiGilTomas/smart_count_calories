e70541628fea89f7d38d81fabc1a4bf81020ce3d
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-08-18 18:39:19 +0200

    Added equivalent measures

--- lib/components/nutrition.dart
+++ lib/components/nutrition.dart
@@ -4,13 +4,14 @@ import 'package:openfoodfacts/utils/NutrimentsHelper.dart';
 import 'package:smart_count_calories/components/item_nutrition.dart';
 
 class Nutrition extends StatelessWidget {
-  const Nutrition({
-    @required this.nutriments,
-    @required this.gramos,
-  });
+  const Nutrition(
+      {@required this.nutriments,
+      @required this.quantity,
+      @required this.measureFactor});
 
   final Nutriments nutriments;
-  final double gramos;
+  final double quantity;
+  final double measureFactor;
 
   @override
   Widget build(BuildContext context) {
@@ -26,45 +27,55 @@ class Nutrition extends StatelessWidget {
         ItemNutrition(
           topNameLine1: 'Energía',
           bottomName: 'calorías',
-          value: (NutrimentsHelper.getEnergyAsKCal(nutriments) * gramos / 100)
+          value: (NutrimentsHelper.getEnergyAsKCal(nutriments) *
+                  quantity /
+                  100 *
+                  measureFactor)
               .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Grasas',
-          value: ((nutriments.fat ?? 0) * gramos / 100).toStringAsFixed(2),
+          value: ((nutriments.fat ?? 0) * quantity / 100 * measureFactor)
+              .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Grasas',
           topNameLine2: 'saturadas',
-          value: ((nutriments.saturatedFat ?? 0) * gramos / 100)
-              .toStringAsFixed(2),
+          value:
+              ((nutriments.saturatedFat ?? 0) * quantity / 100 * measureFactor)
+                  .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Hidratos de',
           topNameLine2: 'carbono',
-          value: ((nutriments.carbohydrates ?? 0) * gramos / 100)
-              .toStringAsFixed(2),
+          value:
+              ((nutriments.carbohydrates ?? 0) * quantity / 100 * measureFactor)
+                  .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Azúcares',
-          value: ((nutriments.sugars ?? 0) * gramos / 100).toStringAsFixed(2),
+          value: ((nutriments.sugars ?? 0) * quantity / 100 * measureFactor)
+              .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Fibra',
-          value: ((nutriments.fiber ?? 0) * gramos / 100).toStringAsFixed(2),
+          value: ((nutriments.fiber ?? 0) * quantity / 100 * measureFactor)
+              .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Proteínas',
-          value: ((nutriments.proteins ?? 0) * gramos / 100).toStringAsFixed(2),
+          value: ((nutriments.proteins ?? 0) * quantity / 100 * measureFactor)
+              .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Sal',
-          value: ((nutriments.salt ?? 0) * gramos / 100).toStringAsFixed(2),
+          value: ((nutriments.salt ?? 0) * quantity / 100 * measureFactor)
+              .toStringAsFixed(2),
         ),
         ItemNutrition(
           topNameLine1: 'Sodio',
-          value:
-              ((nutriments.salt ?? 0) * gramos / 100 * 0.4).toStringAsFixed(2),
+          value: ((nutriments.salt ?? 0) * quantity / 100 * 0.4 * measureFactor)
+              .toStringAsFixed(2),
         ),
       ],
     );
--- lib/screens/product.dart
+++ lib/screens/product.dart
@@ -17,6 +17,7 @@ class _ProductCardState extends State<ProductCard> {
   double gramos = 100.0;
 
   final textEditingController = TextEditingController();
+  String selectedMeasure = 'gramos';
   bool showFloatButton = true;
 
   @override
@@ -27,200 +28,206 @@ class _ProductCardState extends State<ProductCard> {
 
   @override
   Widget build(BuildContext context) {
-    List<ProductImage> productImages = widget.product.selectedImages;
-    Nutriments nutriments = widget.product.nutriments;
-    Iterable<MapEntry<String, Level>> levels =
+    final List<ProductImage> productImages = widget.product.selectedImages;
+    final Nutriments nutriments = widget.product.nutriments;
+    final Iterable<MapEntry<String, Level>> levels =
         widget.product.nutrientLevels.levels.entries;
-    Map<String, dynamic> productJson = widget.product.toJson();
-    String isVegan =
+    final Map<String, dynamic> productJson = widget.product.toJson();
+    final String isVegan =
         productJson['ingredients_analysis_tags'][0].toString().substring(3);
-    String isVegetarian =
+    final String isVegetarian =
         productJson['ingredients_analysis_tags'][1].toString().substring(3);
-    String isPalmOilFree =
+    final String isPalmOilFree =
         productJson['ingredients_analysis_tags'][2].toString().substring(3);
+
+    final Map<String, double> equivalentMeasures = {
+      'gramos': 1.0,
+      'ml': 1.0,
+      'taza desayuno': 2.5,
+      'taza té': 1.5,
+      'taza café': 1,
+      'cuchara sopera': 0.15,
+      'cuchara postre': 0.10,
+      'cuchara café': 0.05,
+      'vaso agua': 2.5,
+      'vasito vino': 1,
+      'vasito licor': 0.50
+    };
 
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           elevation: 0.0,
           leading: IconButton(
-              icon: Icon(Icons.arrow_back),
+              icon: const Icon(Icons.arrow_back),
               onPressed: () => Navigator.pop(context)),
-          title: Text('Smart Count Calories'),
+          title: const Text('Smart Count Calories'),
         ),
         body: DefaultTabController(
           length: 3,
           initialIndex: 1,
-          child: Column(
-            children: [
-              Expanded(
-                child: Scaffold(
-                  floatingActionButton: showFloatButton
-                      ? FloatingActionButton.extended(
-                          icon: Icon(Icons.add),
-                          onPressed: () => null,
-                          label: Text(
-                            'Añadir producto',
-                          ),
-                        )
-                      : null,
-                  appBar: AppBar(
-                    leading: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          productImages
-                              .firstWhere((image) =>
-                                  image.size == ImageSize.THUMB &&
-                                  image.field == ImageField.FRONT)
-                              .url,
-                        ),
+          child: Column(children: [
+            Expanded(
+              child: Scaffold(
+                floatingActionButton: showFloatButton
+                    ? FloatingActionButton.extended(
+                        icon: const Icon(Icons.add),
+                        onPressed: () => null,
+                        label: const Text('Añadir producto'),
+                      )
+                    : null,
+                appBar: AppBar(
+                  leading: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        productImages
+                            .firstWhere((image) =>
+                                image.size == ImageSize.THUMB &&
+                                image.field == ImageField.FRONT)
+                            .url,
                       ),
-                    ),
-                    backgroundColor: Theme.of(context).primaryColor,
-                    title: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: widget.product.productName,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18.0,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .bodyText1
-                                    .color),
-                          ),
-                          widget.product.brands != ''
-                              ? TextSpan(
-                                  text: '\n(${widget.product.brands})',
-                                  style: TextStyle(
-                                      color: Theme.of(context)
-                                          .textTheme
-                                          .bodyText1
-                                          .color),
-                                )
-                              : TextSpan(),
-                        ],
-                      ),
-                    ),
-                    centerTitle: true,
-                    elevation: 10.0,
-                    bottom: TabBar(
-                      labelPadding: EdgeInsets.all(5.0),
-                      labelStyle: TextStyle(fontSize: 18.0),
-                      tabs: <Widget>[
-                        Text('Nutricional'),
-                        Text('Puntuación'),
-                        Text('Aditivos'),
-                      ],
                     ),
                   ),
-                  body: TabBarView(
-                    children: [
-                      Column(
-                        children: [
-                          Flexible(
-                            flex: 6,
-                            child: Nutrition(
-                                nutriments: nutriments, gramos: gramos),
-                          ),
-                          Flexible(
-                            flex: 2,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: TextField(
-                                      controller: textEditingController,
-                                      textAlign: TextAlign.center,
-                                      keyboardType: TextInputType.number,
-                                      onTap: () {
-                                        setState(() {
-                                          textEditingController.text =
-                                              textEditingController.text
-                                                  .split('gramos')[1];
-                                          showFloatButton = false;
-                                        });
-                                      },
-                                      onChanged: (value) {
-                                        setState(() {
-                                          if (int.parse(value) > 500) {
-                                            textEditingController.text = '500';
-                                            value = '500';
-                                          }
-                                          gramos = double.parse(value);
-                                        });
-                                      },
-                                      onSubmitted: (value) {
-                                        textEditingController.text =
-                                            '${gramos.toInt()}  gramos';
-                                        setState(() {
-                                          showFloatButton = true;
-                                        });
-                                      }),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Flexible(
-                            flex: 1,
-                            child: Slider(
-                              value: gramos,
-                              max: 500.0,
-                              min: 0.0,
-                              divisions: 500,
-                              onChanged: (double newValue) {
-                                setState(
-                                  () {
-                                    gramos = newValue;
-                                    textEditingController.text =
-                                        '${newValue.toInt()} gramos';
-                                  },
-                                );
-                              },
-                            ),
-                          )
-                        ],
+                  backgroundColor: Theme.of(context).primaryColor,
+                  title: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(children: [
+                      TextSpan(
+                        text: widget.product.productName,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: Theme.of(context).textTheme.bodyText1.color),
                       ),
-                      Score(
-                          nutriscore: widget.product.nutriscore,
-                          nutriments: nutriments,
-                          levels: levels,
-                          isVegan: isVegan,
-                          isVegetarian: isVegetarian,
-                          isPalmOilFree: isPalmOilFree),
-                      ListView(
-                        children: [
-                          Column(
-                            children: [
-                              Text('Aditivos: '),
-                              for (var additive
-                                  in widget.product.additives.names)
-                                Text(
-                                  '$additive ',
-                                ),
-                              Text('Alérgenos: '),
-                              for (var allergen
-                                  in widget.product.allergens.names)
-                                Text(
-                                  '$allergen ',
-                                ),
-                              Text('Contiene trazas de: '),
-                              for (var tag in widget.product.tracesTags)
-                                Text(
-                                  '${tag.substring(3)} ',
-                                ),
-                            ],
-                          )
-                        ],
-                      ),
+                      widget.product.brands != ''
+                          ? TextSpan(
+                              text: '\n(${widget.product.brands})',
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1
+                                      .color),
+                            )
+                          : const TextSpan(),
+                    ]),
+                  ),
+                  centerTitle: true,
+                  elevation: 10.0,
+                  bottom: const TabBar(
+                    labelPadding: EdgeInsets.all(5.0),
+                    labelStyle: TextStyle(fontSize: 18.0),
+                    tabs: <Widget>[
+                      Text('Nutricional'),
+                      Text('Puntuación'),
+                      Text('Aditivos'),
                     ],
                   ),
                 ),
+                body: TabBarView(children: [
+                  Column(children: [
+                    Flexible(
+                      flex: 7,
+                      child: Nutrition(
+                        nutriments: nutriments,
+                        quantity: gramos,
+                        measureFactor: equivalentMeasures[selectedMeasure],
+                      ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: Row(children: [
+                        PopupMenuButton(
+                            onSelected: (String measure) => setState(() {
+                                  selectedMeasure = measure;
+                                  textEditingController.text =
+                                      '${gramos.toInt()} $selectedMeasure';
+                                }),
+                            itemBuilder: (context) => equivalentMeasures.keys
+                                .map((measure) => PopupMenuItem(
+                                      child: Text('$measure'),
+                                      value: measure,
+                                    ))
+                                .toList()),
+                        Expanded(
+                          child: TextField(
+                              decoration: InputDecoration(isDense: true),
+                              controller: textEditingController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              onTap: () {
+                                setState(() {
+                                  textEditingController.text =
+                                      textEditingController.text
+                                          .split('$selectedMeasure')[1];
+                                  showFloatButton = false;
+                                });
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  if (int.parse(value) > 500) {
+                                    textEditingController.text = '500';
+                                    value = '500';
+                                  }
+                                  gramos = double.parse(value);
+                                });
+                              },
+                              onSubmitted: (value) {
+                                textEditingController.text =
+                                    '${gramos.toInt()}  $selectedMeasure';
+                                setState(() {
+                                  showFloatButton = true;
+                                });
+                              }),
+                        ),
+                        RaisedButton(
+                          onPressed: () => {},
+                          child: Text('Ración'),
+                        )
+                      ]),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Slider(
+                          value: gramos,
+                          max: 500.0,
+                          min: 0.0,
+                          divisions: 500,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              gramos = newValue;
+                              textEditingController.text =
+                                  '${newValue.toInt()} $selectedMeasure';
+                            });
+                          }),
+                    )
+                  ]),
+                  Score(
+                      nutriscore: widget.product.nutriscore,
+                      nutriments: nutriments,
+                      levels: levels,
+                      isVegan: isVegan,
+                      isVegetarian: isVegetarian,
+                      isPalmOilFree: isPalmOilFree),
+                  ListView(children: [
+                    Column(children: [
+                      const Text('Aditivos: '),
+                      for (var additive in widget.product.additives.names)
+                        Text('$additive '),
+                      const Text('Alérgenos: '),
+                      for (var allergen in widget.product.allergens.names)
+                        Text('$allergen '),
+                      const Text('Contiene trazas de: '),
+                      for (var tag in widget.product.tracesTags)
+                        Text(
+                          '${tag.substring(3)} ',
+                        ),
+                    ])
+                  ]),
+                ]),
               ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
