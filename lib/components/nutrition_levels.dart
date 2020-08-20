a67fb2dfdb299b7a1d91da25e736d444bf63768c
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-08-20 10:32:57 +0200

    Refactor tags and translate

--- lib/components/nutrition_levels.dart
+++ lib/components/nutrition_levels.dart
@@ -27,7 +27,7 @@ class NutritionLevels extends StatelessWidget {
                       child: Padding(
                         padding: EdgeInsets.all(5.0),
                         child: Text(
-                          'Nutrition Levels',
+                          'Valores nutricionales',
                           textAlign: TextAlign.center,
                           style: TextStyle(
                               fontSize: 18.0,
@@ -52,36 +52,18 @@ class NutritionLevels extends StatelessWidget {
                         Flexible(
                           fit: FlexFit.tight,
                           child: Text(
-                            '${level.key.toUpperCase()}',
+                            tagText(level.key),
                             textAlign: TextAlign.center,
                           ),
                         ),
                         Flexible(
                           fit: FlexFit.tight,
                           child: Text(
-                            '${level.value.value.toUpperCase()}',
+                            tagLevel(level.value.value),
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                                color: level.value.value == 'low'
-                                    ? Colors.green[Theme.of(context)
-                                                .brightness
-                                                .toString() ==
-                                            'Brightness.dark'
-                                        ? 200
-                                        : 800]
-                                    : level.value.value == 'moderate'
-                                        ? Colors.orange[Theme.of(context)
-                                                    .brightness
-                                                    .toString() ==
-                                                'Brightness.dark'
-                                            ? 200
-                                            : 800]
-                                        : Colors.red[Theme.of(context)
-                                                    .brightness
-                                                    .toString() ==
-                                                'Brightness.dark'
-                                            ? 200
-                                            : 800],
+                                color: tagColor(
+                                    tag: level.value.value, context: context),
                                 fontWeight: FontWeight.bold),
                           ),
                         ),
@@ -95,5 +77,53 @@ class NutritionLevels extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  String tagText(String tag) {
+    switch (tag) {
+      case 'sugars':
+        return 'Azúcares';
+        break;
+      case 'fat':
+        return 'Grasas';
+        break;
+      case 'saturated-fat':
+        return 'Grasas saturadas';
+        break;
+      default:
+        return 'Sal';
+    }
+  }
+
+  String tagLevel(String tag) {
+    switch (tag) {
+      case 'high':
+        return 'Cantidad elevada';
+        break;
+      case 'medium':
+        return 'Cantidad moderada';
+        break;
+      default:
+        return 'Cantidad baja';
+    }
+  }
+
+  Color tagColor({String tag, BuildContext context}) {
+    switch (tag) {
+      case 'high':
+        return Colors.red[calculateBrightness(context)];
+        break;
+      case 'medium':
+        return Colors.orange[calculateBrightness(context)];
+        break;
+      default:
+        return Colors.green[calculateBrightness(context)];
+    }
+  }
+
+  int calculateBrightness(BuildContext context) {
+    return Theme.of(context).brightness.toString() == 'Brightness.dark'
+        ? 200
+        : 800;
   }
 }
